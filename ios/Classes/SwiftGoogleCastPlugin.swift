8e14e6a2fddea13be0605f9eff216c1a9e618c59
Kolo13 <kori.kolodziejczak@gmail.com>
2025-04-21 18:33:33 -0700

    build.gradle namespace; SwiftGoogleCastPlugin.swift refactor

--- ios/Classes/SwiftGoogleCastPlugin.swift
+++ ios/Classes/SwiftGoogleCastPlugin.swift
@@ -2,87 +2,53 @@ import Flutter
 import UIKit
 import GoogleCast
 
-public class SwiftGoogleCastPlugin:GCKCastContext, GCKLoggerDelegate, FlutterPlugin, UIApplicationDelegate    {
+public class SwiftGoogleCastPlugin: NSObject, GCKLoggerDelegate, FlutterPlugin, UIApplicationDelegate {
+
     let kDebugLoggingEnabled = true
-    private var channel : FlutterMethodChannel?
-   
-    
-    public override var sessionManager: GCKSessionManager {
-        GCKCastContext.sharedInstance().sessionManager
+    private var channel: FlutterMethodChannel?
+
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let instance = SwiftGoogleCastPlugin()
+
+        instance.channel = FlutterMethodChannel(name: "google_cast.context", binaryMessenger: registrar.messenger())
+        registrar.addMethodCallDelegate(instance, channel: instance.channel!)
+
+        FGCSessionManagerMethodChannel.register(with: registrar)
+        FGCSessionMethodChannel.register(with: registrar)
+        FGCDiscoveryManagerMethodChannel.register(with: registrar)
+        RemoteMediaClienteMethodChannel.register(with: registrar)
     }
-    public override var discoveryManager: GCKDiscoveryManager {
-        GCKCastContext.sharedInstance().discoveryManager
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "setSharedInstanceWithOptions":
+            setSharedInstanceWithOption(arguments: call.arguments as! [String: Any], result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
 
-    //MARK: - RegisterMethodChannel
-  public static func register(with registrar: FlutterPluginRegistrar) {
-   
-      let instance = SwiftGoogleCastPlugin()
-      
-      instance.channel = FlutterMethodChannel(name: "google_cast.context", binaryMessenger: registrar.messenger())
-    
-      registrar.addMethodCallDelegate(instance, channel: instance.channel!)
-      FGCSessionManagerMethodChannel.register(with: registrar)
-      FGCSessionMethodChannel.register(with: registrar)
-      FGCDiscoveryManagerMethodChannel.register(with: registrar)
-      RemoteMediaClienteMethodChannel.register(with: registrar)
-      
-      
-      
-    
-  }
-
-    
-    //MARK: - FlutterPlugin
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-
-      switch call.method {
-      case "setSharedInstanceWithOptions":
-          setSharedInstanceWithOption(arguments: call.arguments as! Dictionary<String, Any>, result: result)
-          break
-      default:
-          break
-      }
-
-      
-  }
-    
-    
-    
-    
-    private func setSharedInstanceWithOption(arguments: Dictionary<String, Any> ,result: @escaping FlutterResult){
+    private func setSharedInstanceWithOption(arguments: [String: Any], result: @escaping FlutterResult) {
         let option = GCKCastOptions.fromMap(arguments)
         GCKCastContext.setSharedInstanceWith(option)
-        GCKLogger.sharedInstance().consoleLoggingEnabled = true
-        GCKLogger.sharedInstance().delegate = self
-        discoveryManager.add(FGCDiscoveryManagerMethodChannel.instance)
-        sessionManager.add(FGCSessionManagerMethodChannel.instance )
-        discoveryManager.startDiscovery()
 
+        let context = GCKCastContext.sharedInstance()
+        GCKLogger.sharedInstance().delegate = self
+        GCKLogger.sharedInstance().consoleLoggingEnabled = true
+
+        context.discoveryManager.add(FGCDiscoveryManagerMethodChannel.instance)
+        context.sessionManager.add(FGCSessionManagerMethodChannel.instance)
+        context.discoveryManager.startDiscovery()
+
+        result(nil)
     }
-    
-    
-    //MARK: - GCKLoggerDelegate
+
+    // MARK: - GCKLoggerDelegate
 
     public func logMessage(_ message: String,
-                      at level: GCKLoggerLevel,
-                      fromFunction function: String,
-                      location: String) {
-
-          print(function + " - " + message)
-        
-      }
-    
-  
-
-    
-    
-    
-    
-    
-    
+                           at level: GCKLoggerLevel,
+                           fromFunction function: String,
+                           location: String) {
+        print("[GCKLogger] \(function): \(message)")
+    }
 }
-
-
-
-
